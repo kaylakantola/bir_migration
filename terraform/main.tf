48889597b0eaabb73634304b48b98d2e27cab6c6
kaylakantola <kkantola@trhc.com>
2020-09-18 12:55:08 -0400

    cloud fns

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,3 +56,62 @@ resource google_storage_bucket arrival_bucket {
   location = "US"
   project  = var.gcp_project_id
 }
+
+data google_storage_project_service_account gcs_account {
+}
+
+resource google_pubsub_topic_iam_binding binding {
+  topic   = module.arrival_pubsub.topic_id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
+
+resource google_storage_notification arrival_notification {
+  bucket         = google_storage_bucket.arrival_bucket.name
+  payload_format = "JSON_API_V1"
+  topic          = module.arrival_pubsub.topic_id
+  event_types    = ["OBJECT_FINALIZE"]
+  depends_on = [google_pubsub_topic_iam_binding.binding]
+}
+
+## CLOUD FUNCTIONS
+
+resource google_storage_bucket cloud_fn_bucket {
+  name = "cloud-fn-bucket"
+}
+
+resource google_storage_bucket_object archive {
+  name   = "index.zip"
+  bucket = google_storage_bucket.cloud_fn_bucket.name
+  source = "./cloud_functions/output/index.zip"
+}
+
+resource google_cloudfunctions_function liftoff {
+  name        = "liftoff"
+  description = "Listens to the take_flight pubsub, posts to the start_migration pubsub"
+  runtime     = "nodejs10"
+
+  source_archive_bucket = google_storage_bucket.cloud_fn_bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "liftoff"
+}
+
+resource google_cloudfunctions_function_iam_member invoker {
+  project        = google_cloudfunctions_function.liftoff.project
+  region         = google_cloudfunctions_function.liftoff.region
+  cloud_function = google_cloudfunctions_function.liftoff.name
+
+  event_trigger = {
+    event_type = "google.pubsub.topic.publish"
+    resource = module.take_flight_pubsub.topic_id
+  }
+
+  environment_variables = {
+    UNSPLASH_ACCESS_KEY = "QjEVYBA0V2FpmylQX3c-f2RIgo7DRo6Z4WLPmgRvcrY"
+    PUBSUB_TOPIC = module.start_migration_pubsub.topic_id
+  }
+  
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
